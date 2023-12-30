621be5cdc6025764cb71a517a24a1091b58456f0
Adam Erickson <adam.michael.erickson@gmail.com>
2023-12-30 09:58:50 -0800

    update

--- 2023/problem_01/run_all.sh
+++ 2023/problem_01/run_all.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
+BENCHMARK=false
+VALIDATE=false
+SOLUTION=0
+
 DIRNAME="$(basename $(realpath $(dirname $0)))"
 echo "Running comparison: $DIRNAME"
 
-(cd ./python && python main.py)
-(cd ./julia && julia main.jl)
-(cd ./cpp && make all && ./main)
-(cd ./rust && cargo build --release && ./main)
+# compile C++ and Rust code
+(cd ./cpp && make all)
+(cd ./rust && cargo build --release)
+
+if [ $VALIDATE = true ]; then
+    PYTHON="$(cd ./python && python main.py)"
+    JULIA="$(cd ./julia && julia main.jl)"
+    CPP="$(cd ./cpp && ./main)"
+    RUST="$(cd ./rust && ./main)"
+
+    if [ $PYTHON = $SOLUTION && $JULIA = $SOLUTION && $CPP = $SOLUTION && $RUST = $SOLUTION ]; then
+        echo 'Validation succeeded.'
+    else
+        echo 'Validation failed.'
+    fi
+fi
+
+if [ $BENCHMARK = true ]; then
+    PYTHON="$(cd ./python && time 'python main.py')"
+    JULIA="$(cd ./julia && time 'julia main.jl')"
+    CPP="$(cd ./cpp && make all && time './main')"
+    RUST="$(cd ./rust && cargo build --release && time './main')"
+fi
 
 echo 'Comparison complete.'
